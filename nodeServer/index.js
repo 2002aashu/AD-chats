adc884284ccc863ba562f1956cd33986f738da13
Your Name <you@example.com>
2024-11-18 11:58:36 +0530

    fisrt version of our chat

---
+++ nodeServer/index.js
@@ -0,0 +1,26 @@
+//NODE SERVER WHICH WILL HANDLE SOCKET IN CONNECTION
+const io = require('socket.io')(7000,{
+    cors: {
+        origin:"*",
+    }
+})
+
+
+const user = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined' , name =>{
+        console.log("new user", name);
+        user[socket.id] = name;
+        socket.broadcast.emit('user-joined', name)
+    });
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
+    });
+
+
+socket.on('disconnect', message => {
+    socket.broadcast.emit('left', user[socket.id]);
+    delete user[socket.id];
+    });
+})
